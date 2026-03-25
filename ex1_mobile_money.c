6f9ba75ea4a902c1418c6250052ba8fccaa404b4
chelangatrobin20-lgtm <chelangatrobin20@gmail.com>
2026-03-25 06:40:53 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,8 +1,24 @@
-o/* Name: CHELANGAT ROBIN */
+/* Name: CHELANGAT ROBIN */
 /* Student Number: 25/U/BIE/05290/PE */
 #include <stdio.h>
 int main() {
-    float amount, fee_percent, fee, total;
+    int amount_to_send;
+    float transaction_fee;
+    int total_aamount_deducted;
+
+
+   printf("ENTER AMOUNT TO SEND :  ");
+    scanf("%d", &amount_to_send);
+
+    printf("ENTER TRANSACTION FEE :  ");
+    scanf("%f", &transaction_fee);
+
+    float fee= transaction_fee*amount_to_send/100;
+
+    printf("YOUR FEE AMOUNT IS : %.1f and  ",fee);
+
+    float amount_deducted = amount_to_send+fee;
+    printf("AMOUNT DEDUCTED IS : %.1f", amount_deducted);
 
     printf("Enter amount to send: ");
     scanf("%f", &amount);
